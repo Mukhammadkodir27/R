87acb335be2a86109725e041dad8b7e13d40145a
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-10-11 18:47:10 +0200

    Programmed all tasks in a complex level, advanced functioning and clean coding

---
+++ R_UW_Classes/HW1/tasks.r
@@ -0,0 +1,31 @@
+#Task 1
+random_num <<- 5.5; print(random_num); random_num_int <<- as.integer(random_num); print(random_num_int); as.character(random_num_int) ->> random_num_char; print(random_num_char)
+
+#Task 2
+string_var1 = "Random Values from Mukhammadkodir"; string_var2 = "Selected values from me too"; print(paste(string_var1, string_var2)); vectorized_value1 <<- "Hahah welcome dear"; vectorized_value2 <<- "Ooops, yay"; c(vectorized_value1, vectorized_value2) ->> combined_vector_values; print(combined_vector_values); print("No idea if i could get it, but i tried XD."); print("From the outputs, the differences are obvious, right?")
+
+#Task 4
+c(2:8, 17:30) ->> vec1; print(vec1)
+
+#Task 5
+vec2 <<- seq(from = 2, to = 32, by = 6); print(vec2)
+
+#Task 6
+rep(c("2", "7", "a"), times = 3) ->> repeated_vector; print(repeated_vector)
+
+#Task 7
+vector_special <- 1:100; vector_special = vector_special[(x %% 3) == 0]; print(vector_special)
+
+#Task 8
+vec3 = rep(c(1, 3, 5, 7, 9), times=c(2, 2, 2, 2, 2)); vec3 = rep(c(vec3), times=3); print(vec3)
+
+#Task 9
+ceiling(c(runif(n=50, min=0, max=20))) ->> vec4; print(vec4)
+
+#Task 10
+print(vec4[c(5, 10, 26)])
+
+#Task 11
+print(vec4[seq(from = 5, to = 50, by = 2)])
+
+
