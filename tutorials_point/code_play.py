034fabf4196c9bf3d5de7e2923ab10dfd5482bd5
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-10-16 17:34:23 +0200

    matrix in r

--- tutorials_point/code_play.py
+++ tutorials_point/code_play.py
@@ -6,3 +6,16 @@ introduce <- function(name){
 
 introduce(name)
 
+
+this_matrix <- matrix(c(1, 2, 3, 4, 5, 6), nrow = 3, ncol = 3)
+
+print(this_matrix)
+
+
+this_matrix <- matrix(c("apple", "banana", "cherry", "oranges"), nrow=2, ncol=2)
+print(this_matrix)
+
+
+print(this_matrix[1, 2])
+
+print(this_matrix[2,])
