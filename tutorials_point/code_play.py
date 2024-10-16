de77903fb876cd1f761176f0de3246eee883231d
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-10-17 00:43:08 +0200

    Update code_play.py

--- tutorials_point/code_play.py
+++ tutorials_point/code_play.py
@@ -19,3 +19,48 @@ print(this_matrix)
 print(this_matrix[1, 2])
 
 print(this_matrix[2,])
+
+
+this_matrix <- matrix(c(1, 2, 3, 4, 5, 6), nrow = 3, ncol = 3)
+
+print(this_matrix)
+
+
+this_matrix <- matrix(c("apple", "banana", "cherry", "oranges"), nrow=2, ncol=2)
+print(this_matrix)
+
+
+print(this_matrix[1, 2])
+
+print(this_matrix[2,])
+
+
+this_matrix <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8, 9), nrow=3, ncol=3)
+print(this_matrix)
+
+print(this_matrix[c(1,2), ])
+print(this_matrix[, c(1,2)])
+
+
+
+new_matrix <- cbind(this_matrix, c(0, 0, 0))
+print(new_matrix)
+
+new_matrix <- rbind(new_matrix, c(0, 0, 0, 0))
+print(new_matrix)
+
+
+
+thismatrix <- matrix(c("apple", "banana", "cherry", "orange", "mango", "pineapple"), nrow = 3, ncol =2)
+#Remove the first row and the first column
+thismatrix <- thismatrix[-c(1), -c(1)]
+print(thismatrix)
+
+
+random_matrix <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8, 9), nrow=3, ncol=3)
+print(random_matrix)
+#removing some selected values from rows and cols
+random_matrix <- random_matrix[-c(1,2), -c(3)]
+print(random_matrix)
+
+
