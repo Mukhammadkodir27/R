00bd26938bd1e6c375caae442a2a5377d92dc160
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-10-18 04:09:21 +0200

    Update factor.r

--- tutorials_point/Data_Structures/factor.r
+++ tutorials_point/Data_Structures/factor.r
@@ -1,1 +1,17 @@
 # R Factors
+# R Factors
+# Factors are used to categorize data. Examples of factors are: 
+# Demography: Male/Female
+# Music: Rock, Pop, Classic, Jazz
+# Training: Strength, Stamina
+
+# to create a factor, use the factor() function and add a vector as argument: 
+music_genre <- factor(c("Jazz", "Rock", "Classic", "Classic", "Pop", "Jazz", "Rock", "Jazz"))
+print(music_genre)
+
+# from the example, you can see that factor has four levels(categories): Classic, Jazz, Pop and Rock
+
+# to only print the levels, use the levels() function: 
+print(levels(music_genre))
+
+# You can also set the levels, by adding the levels argument inside the factor() function: 
