3bb5a8dd508597366755e90658adbeb23197383e
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-10-20 19:26:50 +0200

    Homework2

--- R_UW_Classes/HW2/exercises.r
+++ R_UW_Classes/HW2/exercises.r
@@ -1,1 +1,9 @@
-# waiting for new tasks
+# 3a
+table<-matrix(0,nrow=5,ncol=2);table[,1]<-3
+# 3b
+table<-matrix(0,nrow=5,ncol=2);table[3,2]<-20
+# 4 
+sampleString<-"Mukhammadkodir";sampleMatrix<-matrix(c(1,0),nrow=2,ncol=2);sampleNumeric<-27;sampleVector<-c(T,F,T,F,F,F,F,T,T,F,T);myList<-list(sampleString,sampleMatrix,sampleNumeric,sampleVector);last_element<-length(myList);myList[[last_element]][4]
+
+# 4d
+sampleString<-"Mukhammadkodir";sampleMatrix<-matrix(c(1,0),nrow=2,ncol=2);sampleNumeric<-27;sampleVector<-c(T,F,T,F,F,F,F,T,T,F,T);myList<-list(sampleString,sampleMatrix,sampleNumeric,sampleVector);last_element<-length(myList);myList[[last_element]][5]<-NA;print(myList[[4]])
